ac0765c91cce4d6a4b62362842b206ab72aac256
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2015-10-19 14:31:33 +0200

    Auto generated id

--- Lab1-JPA-Netbeans/src/pl/polsl/gabrys/arkadiusz/model/Author.java
+++ Lab1-JPA-Netbeans/src/pl/polsl/gabrys/arkadiusz/model/Author.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
@@ -20,6 +22,7 @@ public class Author implements Serializable {
      * Author unique id
      */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     /**
--- Lab1-JPA-Netbeans/src/pl/polsl/gabrys/arkadiusz/model/Book.java
+++ Lab1-JPA-Netbeans/src/pl/polsl/gabrys/arkadiusz/model/Book.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -20,6 +22,7 @@ public class Book implements Serializable {
      * Book unique id
      */
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     /**
